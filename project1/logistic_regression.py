fix gradient

project1/logistic_regression.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tqdm
+import pandas as pd
 
 class LogisticRegression():
     """
@@ -12,43 +13,61 @@ class LogisticRegression():
         self.batch_size = batch_size
         self.verbose = verbose
         self.regularization_lambda = regularization_lambda
+        self.train_loss = []
+        self.val_loss = []
+        self.bias = 0.0
 
         self.weights = None
 
-    def fit(self, X, Y):
+    def fit(self, X, Y, X_val=None, Y_val=None):
         """
             Train the logistic regression model using stochastic gradient descent.
         """
         self.weights = np.zeros(X.shape[1]) # 1 weight per feature
-        for e in range(self.num_epochs):
-            random_indices = np.random.permutation((len(X)))[:self.batch_size]
-            X_batch = X[random_indices]
-            Y_batch = Y[random_indices]
-            self.weights = self.weights - self.learning_rate * self.gradient(X_batch, Y_batch)
-        
-        # raise NotImplementedError("Not implemented yet")
+        for _ in range(self.num_epochs):
+            shuffle = np.random.permutation((len(X)))
+            X_shuffled = X[shuffle]
+            Y_shuffled = Y[shuffle]
+            for i in np.arange(0, len(X), self.batch_size):
+                X_batch = X_shuffled[i : i + self.batch_size]
+                Y_batch = Y_shuffled[i : i + self.batch_size].reshape(-1)
+                # (f"X_b {X_batch.shape}, Y_b {Y_batch.shape}")
+                self.weights -= self.learning_rate * self.gradient(X_batch, Y_batch)
+                self.bias -= self.learning_rate * np.mean(self.predict_proba(X_batch) - Y_batch)
+
+            self.train_loss.append(self.CELoss(Y, self.predict_proba(X)))
+            if X_val is not None and Y_val is not None:
+                self.val_loss.append(self.CELoss(Y_val, self.predict_proba(X_val)))
 
     def gradient(self, X, Y):
         """
             Compute the gradient of the loss with respect to theta and bias with L2 Regularization.
             Hint: Pay special attention to the numerical stability of your implementation.
         """
-        # raise NotImplementedError("Not implemented yet")
         P = self.predict_proba(X)
-        return (P - Y) @ X + 2 * self.regularization_lambda * self.weights
+        # print(f"P {P.shape} X.T {X.T.shape} grad {(X.T @ (P - Y)).shape}")
+        return (1/len(Y)) * X.T @ (P - Y) + 2 * self.regularization_lambda * self.weights
 
     def predict_proba(self, X):
         """
             Predict the probability of lung cancer for each sample in X.
         """
-        # raise NotImplementedError("Not implemented yet")
         sigmoid = lambda z: 1 / (1 + np.exp(-z))
-        return sigmoid(X @ self.weights)
+        # print(f"Wx + b {(X@self.weights).shape}")
+        return sigmoid(X @ self.weights + self.bias)
 
 
     def predict(self, X, threshold=0.5):
         """
             Predict the if patient will develop lung cancer for each sample in X.
         """
-        # raise NotImplementedError("Not implemented yet")
         return (self.predict_proba(X) >= threshold).astype(int)
+    
+    def CELoss(self, target, prob):
+        eps = 1e-9
+        return -np.mean(target*np.log(prob + eps) + (1 - target)*np.log(1-prob + eps))
+
+    def get_loss_csv(self):
+        epochs = 1 + np.arange(self.num_epochs)
+        loss_df = pd.DataFrame({"epoch": epochs, "train_loss": self.train_loss, "val_loss":self.val_loss})
+        loss_df.to_csv("losses.csv")

project1/vectorizer.py
@@ -17,7 +17,7 @@ class Vectorizer:
         :return: function to map numerical x to a zero mean, unit std dev normalized score.
         """
         values = np.array([v for v in values if not Vectorizer.is_missing(v)]).astype(float)
-        mean, std = np.mean(values), np.std(values)
+        mean, std = np.mean(values), np.std(values, ddof=1)
 
         # raise NotImplementedError("Numerical vectorizer not implemented yet")
 
@@ -29,7 +29,6 @@ class Vectorizer:
             Hint: this fn knows mean and std from the outer scope
             """
             # Standard space: z-score = (x - mu)/sigma
-
             if Vectorizer.is_missing(x):
                 x = mean
 
@@ -140,7 +139,10 @@ class Vectorizer:
             #transformed_data.append(transformed_datapoint)
             transformed_data.append(row)
 
-        return np.array(transformed_data)
+        transformed_data = np.array(transformed_data)
+        # bias = np.ones((transformed_data.shape[0], 1))
+        # return np.hstack([bias, transformed_data])
+        return transformed_data
     
 
     def is_missing(val):